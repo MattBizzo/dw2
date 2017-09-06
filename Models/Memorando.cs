e7e06768d20270d2e9a831536bc4e769888c4496
adillls <adilsons.porto@gmail.com>
2017-09-05 21:15:56 -0300

    Add files via upload
    Add 2 models

---
+++ Models/Memorando.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memos.Models
+{
+    public class Memorando
+    {
+
+        public string Logo { get; set; }
+
+        public string Local { get; set; }
+
+        public  string Data { get; set; }
+
+        public int Id { get; set; }
+
+        public string Setor { get; set; }
+
+        public string Assunto { get; set; }
+    }
+}
